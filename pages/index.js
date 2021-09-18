3aded03943324717b34baa15dfd9deaf43709b92
Israel Sousa <me@israelsousa.com.br>
2021-09-18 11:33:06 -0300

    feat: Configurando DarkMode

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,37 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
+
+import Head from 'next/head';
+//import styles from '../styles/Home.module.css';
+import styled from 'styled-components';
+import TopBar from '../components/TopBar';
 
 export default function Home() {
+  const [isDarkMode, setDarkMode] = useState(false);
+
+  const handleToggle = () => {
+    setDarkMode(!isDarkMode);
+  }
+
   return (
-    <div className={styles.container}>
+    <Page dark={isDarkMode ? true : false}>
       <Head>
         <title>Promobit - Filmes</title>
         <meta name="description" content="Os melhores filmes do TMDB" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
-    </div>
+      <TopBar dark={isDarkMode ? true : false}
+              toggle={handleToggle}
+      />
+    </Page>
   )
 }
+
+const Page = styled.div`
+  position: relative;
+  min-height: 100vh;
+  width: 100vw;
+  display: flex;
+  transition: .5s;
+  background: ${props => props.dark ? "#000" : "#fff"};
+`;
