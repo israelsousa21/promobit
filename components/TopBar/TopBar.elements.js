88d4db9c13fcc0446bb77b21986b05d22ffbd693
Israel Sousa <me@isaelsousa.com.br>
2021-10-11 07:32:16 -0300

    1 - feat: add TopBar component

---
+++ components/TopBar/TopBar.elements.js
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+import {Container} from '../../styles/globalStyles';
+
+export const Section = styled(Container)`
+    position: fixed;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100px;
+    z-index: 9999;
+    padding: 0;
+            
+    ${Container}
+`;
+
+export const Blurbar = styled.div `
+    position: fixed;
+    width: 100%;
+    filter: blur(3px);
+    -webkit-filter: blur(3px);
+    opacity: 0.5;
+    min-height:100px;        
+    box-shadow: 0 0 0 5px ${props => props.dark ? "#000" : "#fff"};
+    background: ${props => props.dark ? "#000" : "#fff"};
+`;
+
+export const Content = styled.nav `
+    z-index:9999;
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    margin-top:20px;
+`;
+
+export const Logo = styled.div `
+    display: flex;
+    flex:1;
+    flex-direction: column;
+    justify-content: center;
+    text-align:center;
+    filter: blur(0px);
+    -webkit-filter: blur(0px);
+    color: ${props => props.dark ? "#fff" : "#000"};
+`;
+export const ToggleButton = styled.a`
+  display: flex;
+  justify-content: center;
+  padding: 15px;
+  filter: blur(0px);
+    -webkit-filter: blur(0px);
+    cursor: pointer;
+`;
---
+++ components/TopBar/TopBar.js
@@ -0,0 +1,29 @@
+import { FaSun } from 'react-icons/fa';
+import { FaMoon } from 'react-icons/fa';
+import Image from 'next/image';
+import LogoPromobit from '../../public/promobit-logo.svg';
+import { Section, 
+         Blurbar, 
+         Content, 
+         Logo, 
+         ToggleButton 
+        } from './TopBar.elements' 
+
+function TopBar(props){
+    
+    return (
+            <Section>
+                <Blurbar dark={props.dark} />
+                    <Content>
+                        <Logo dark={props.dark}>
+                            <Image src={LogoPromobit} alt="Logo Promobit" />
+                        </Logo>
+                        <ToggleButton onClick={props.toggle}>
+                            { props.dark ? <FaSun color={'#fff'} size={20} /> : <FaMoon color={'#000'} size={20} /> }
+                        </ToggleButton>
+                    </Content>
+            </Section>
+    )
+}
+
+export default TopBar;
